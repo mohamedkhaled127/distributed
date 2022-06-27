2ad41f6b663c3fd25b0dcfbdb796b81338bff83c
Khaled amr ismail <72163567+Vnspec2@users.noreply.github.com>
2022-06-27 19:41:08 +0200

    Add files via upload

---
+++ 4/src/dao/UserQueries.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import model.User;
+
+/**
+ *
+ * @author mohamedkhaled
+ */
+public class UserQueries {
+    
+    /*public static void save(User user)
+    {
+        String query="insert into user(name,email,mobileno,password,status) values(user.getName(),user.getEmail(),user.getMobileno(),user.getPassword(),user.getStatus()"
+    }*/
+    
+}
